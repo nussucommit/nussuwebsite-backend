c83dde11a68b59e3106c0e5236926e91285233a0
LouisLouis19 <andreaslouis.louis@gmail.com>
2023-04-08 12:06:56 +0800

    Create views for remaining about us pages

--- backend/aboutus/urls.py
+++ backend/aboutus/urls.py
@@ -3,4 +3,8 @@ from aboutus import views
 
 urlpatterns = [
   path('aboutus/', views.aboutus),
+  path('history/', views.history),
+  path('governance/', views.governance),
+  path('ourteam/', views.ourteam),
+  path('president/', views.president),
 ]
