712d9c16c171f96cec29311abded0cdc42e6f11f
LouisLouis19 <andreaslouis.louis@gmail.com>
2023-04-08 11:44:56 +0800

    Create home app and views

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from notionConnection.views import TestAPIView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', TestAPIView.as_view())
-
+    path('test/', TestAPIView.as_view()),
+    path('', include('home.urls')),
 ]
