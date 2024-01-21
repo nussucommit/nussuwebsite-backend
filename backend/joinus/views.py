58131816268e7a3a19a6b9bf4d216de931965ae4
Stanley Wijaya <wijayastanley00@gmail.com>
2024-01-21 20:29:28 +0800

    Add cache for join us page

--- backend/joinus/views.py
+++ backend/joinus/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from django.core.cache import cache
 from dotenv import load_dotenv
 from rest_framework.response import Response
 from rest_framework import status
@@ -19,7 +20,14 @@ NOTION_HEADER = {'Notion-Version': version, 'Authorization': token}
 @api_view(['Get'])
 def joinus(request):
     JOINUS_URL = 'bda84ac388784fd4a1ad13a78a20ed4a'
-    data = get_parsed_data(JOINUS_URL)
+
+    cache_key = 'joinus'
+    data = cache.get(cache_key)
+
+    if data is None:
+        data = get_parsed_data(JOINUS_URL)
+        cache.set(key=cache_key, value=data, timeout=60 * 30)
+
     return Response(data, status=status.HTTP_200_OK)
 
 def get_parsed_data(id):
