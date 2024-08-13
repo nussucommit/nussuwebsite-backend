3d504dab570c1a62500ebe3136478f05134557ab
Siyi Xu <e1300538@u.nus.edu>
2024-08-13 19:47:28 +0800

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -160,5 +160,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'protechsnutrition@gmail.com'
-EMAIL_HOST_PASSWORD = 'rsac jmnl atdw ywda'  # Note: Store credentials securely
+EMAIL_HOST_USER = 'siyixu2005@gmail.com'
+EMAIL_HOST_PASSWORD = 'ctlh gzpf wxtr pugc'  # Note: Store credentials securely
