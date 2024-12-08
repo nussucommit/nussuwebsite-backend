e8fe32954d834ffa1a8665eb9968328170a89b26
Jason Ray <jasonraymoniaga@gmail.com>
2024-12-08 22:53:01 +0800

    Edit settings to fix email host

--- backend/settings.py
+++ backend/settings.py
@@ -160,5 +160,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'siyixu2005@gmail.com'
-EMAIL_HOST_PASSWORD = 'ctlh gzpf wxtr pugc'  # Note: Store credentials securely
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")  # Note: Store credentials securely
